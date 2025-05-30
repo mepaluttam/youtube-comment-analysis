d79b7e05a15871517c4ae711abe5eca83b0b775f
uttam <uttampal78945@gmail.com>
2025-05-31 00:54:40 +0530

    added mlflow workflow

---
+++ scripts/mlflow_test.py
@@ -0,0 +1,18 @@
+import mlflow
+import random
+
+# Set the MLflow tracking URI
+import dagshub
+dagshub.init(repo_owner='mepaluttam', repo_name='youtube-comment-analysis', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/mepaluttam/youtube-comment-analysis.mlflow/")
+# Start an MLflow run
+with mlflow.start_run():
+    # Log some random parameters
+    mlflow.log_param("param1", random.randint(1, 100))
+    mlflow.log_param("param2", random.random())
+
+    # Log some random metrics
+    mlflow.log_metric("metric1", random.random())
+    mlflow.log_metric("metric2", random.uniform(0.5, 1.5))
+
+    print("Logged random parameters and metrics.")
